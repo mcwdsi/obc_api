b38cf61c418e3a66c842758803cc7582e7a9685b
tvmendoza <tonamendoza@ufl.edu>
2017-05-10 18:32:41 -0400

    Updated the logic to match what the UI expects, still need to deduplicate the rest of the fields

--- sparql_queries/indexing-mdc-harness.js
+++ sparql_queries/indexing-mdc-harness.js
@@ -25,127 +25,125 @@ function IndexingMDCHarness() {
         var con = new stardog.Connection();
         con.setEndpoint(config.stardogURL);
         con.setCredentials(config.stardogUser, config.stardogPass);
-        console.log("WENT HERE")
         fs.readFile(__dirname + '/indexing_terms_queries/retrieval/mdc_software_tree_mdc.rq', function (err, partOf_query_file) {
-            console.log(partOf_query_file)
             con.query({
                 database: config.stardogDB,
                 query: partOf_query_file.toString(),
                 agent: agent
             },
                 function (mdc_data_tree) {
+                    if (mdc_data_tree != undefined){
                         formed_tree = convertToTree(mdc_data_tree);
                         callback(formed_tree);
+                    }
                     });
+
         });
    };
 
-    // this.retrievalQuery = function (callback) {
-        
-    //     utils.getNewURI();
-    //     if (typeof retrievalCachedResults !== 'undefined') {
-    //         callback(retrievalCachedResults);
-    //         this._updateRetrievalCache(function (data) {
-    //             //no op, since we've already returned the cached results
-    //             //this is just to refresh the cache
-    //         });
-    //     } else {
-    //         this._updateRetrievalCache(callback);
-    //     }
-    // };
-
-//     this._updateRetrievalCache = function (callback) {
-//         var con = new stardog.Connection();
-//         con.setEndpoint(config.stardogURL); 
-//         con.setCredentials(config.stardogUser, config.stardogPass);
-
-//         fs.readFile(__dirname + '/indexing_terms_queries/retrieval/partOf_indexing_terms.rq', function (err, partOf_query_file) {
-//             con.query({
-//                 database: config.stardogDB,
-//                 query: partOf_query_file.toString(),
-//                 agent: agent
-//             },
-//                 function (partOf_terms) {
-//                     //get second subset of indexing terms
-//                     fs.readFile(__dirname + '/indexing_terms_queries/retrieval/subClassOf_indexing_terms.rq', function (err, subClassOf_query_file) {
-//                         con.query({
-//                             database: config.stardogDB,
-//                             query: subClassOf_query_file.toString(),
-//                             agent: agent
-//                         },
-//                             function (subClassOf_terms) {
-//                                 fs.readFile(__dirname + '/indexing_terms_queries/retrieval/ecosystem_indexing_terms.rq', function (err, ecosystem_query_file) {
-//                                     con.query({
-//                                         database: config.stardogDB,
-//                                         query: ecosystem_query_file.toString(),
-//                                         agent: agent
-//                                     },
-//                                         function (ecosystem_terms) {
-//                                             retrievalCachedResults = convertToTree([partOf_terms, subClassOf_terms, ecosystem_terms]);
-//                                             callback(retrievalCachedResults);
-//                                         });
-
-//                                 });
-//                             });
-//                     });
-//                 });
-//         });
-//     };
-// };
 
 function convertToTree(queryAnswer) {
     var tree = {};
     var uniqueSoftwaresubs = []
     var results = queryAnswer.results.bindings;
     for (var j in results) {
+        // console.log(j)
+        // console.log(results[j])
         currentItem = {}
         var rootName = results[j].softwareOrSubclassLabel.value
         var parentTermURI = results[j].softwareOrSubclass.value
         var termURI = results[j].x.value
         currentItem[termURI] = {}
-        currentItem[termURI].prefTerm = results[j].prefTerm.value
-        if(results[j].sourceCodeRelease !== undefined){
-            currentItem[termURI].sourceCodeRelease =  results[j].sourceCodeRelease.value
-         }
-        if(results[j].synopsis !== undefined){
-            currentItem[termURI].synopsis = results[j].synopsis.value
-        }
-        if(results[j].executableName !== undefined){
-            currentItem[termURI].execName = results[j].executableName.value
-        }
-        if(results[j].executableUrl !== undefined){
-            currentItem[termURI].execURL = results[j].executableUrl.value
-        }
-        if(results[j].executableUrl !== undefined){
-             currentItem[termURI].execURL = results[j].executableUrl.value
-        }
-        if(results[j].documentationDescription !== undefined){
-             currentItem[termURI].documentationDesc = results[j].documentationDescription.value
-        }
-        if(results[j].documentationUrl !== undefined){
-            currentItem[termURI].documentationURL = results[j].documentationUrl.value
-        }
-        if(results[j].version !== undefined){
-            currentItem[termURI].version = results[j].version.value
-        }
-        if(results[j].sourceCodeRepositoryUrl !== undefined){
-           currentItem[termURI].codeURl = results[j].sourceCodeRepositoryUrl.value
-        }
-        if(results[j].measureLabel !== undefined){
-            currentItem[termURI].measureLabel = results[j].measureLabel.value
-        } 
-        //now we need to construct the tree
+        currentItem[termURI].label = results[j].prefTerm.value
+        currentItem[termURI].isRoot = false
+        currentItem[termURI].children = {}
+        var newURI = true;
+
         if(tree[rootName] == undefined){
             tree[rootName] = []
+        } // if the tree is not empty then we have to take into account that the data could be there twice. 
+        else {
+            for (var i in tree[rootName]) {
+                if(tree[rootName][i][termURI] !== undefined){
+                    if(results[j].developerName !== undefined){
+                        newURI = false;
+                        for (var dev in tree[rootName][i][termURI].developerName) {
+                            tree[rootName][i][termURI].developerName.push(results[j].developerName.value)
+                        }
+                        tree[rootName][i][termURI].developerName = Array.from(new Set(tree[rootName][i][termURI].developerName))
+                    }
+                }
+            }
+           
         }
-        tree[rootName].push(currentItem)
+        // if the URI is new then we do not have to worry about dupliciates
+        if (newURI){
+            // if(results[j].sourceCodeRelease !== undefined){
+            //     currentItem[termURI].sourceCodeRelease =  results[j].sourceCodeRelease.value
+            // }
+            // if(results[j].synopsis !== undefined){
+            //     currentItem[termURI].synopsis = results[j].synopsis.value
+            // }
+            // if(results[j].developerName !== undefined){
+            //     currentItem[termURI].developerName = [results[j].developerName.value]
+            // }
+            // if(results[j].executableName !== undefined){
+            //     currentItem[termURI].execName = results[j].executableName.value
+            // }
+            // if(results[j].executableUrl !== undefined){
+            //     currentItem[termURI].execURL = results[j].executableUrl.value
+            // }
+            // if(results[j].documentationDescription !== undefined){
+            //      currentItem[termURI].documentationDesc = results[j].documentationDescription.value
+            // }
+            // if(results[j].documentationUrl !== undefined){
+            //     currentItem[termURI].documentationURL = results[j].documentationUrl.value
+            // }
+            // if(results[j].version !== undefined){
+            //     currentItem[termURI].version = results[j].version.value
+            // }
+            // if(results[j].sourceCodeRepositoryUrl !== undefined){
+            //    currentItem[termURI].codeURl = results[j].sourceCodeRepositoryUrl.value
+            // }
+            // if(results[j].measureLabel !== undefined){
+            //     currentItem[termURI].measureLabel = results[j].measureLabel.value
+            // }  
+        }
+
+        if (j ==4 ){
+            break;
+        }
+
+        if(newURI){
+            tree[rootName].push(currentItem)
+        }
         //this gets the unique IDs and might be used in the future.
         // if(uniqueSoftwaresubs.indexOf(rootName) < 0) {
         //     uniqueSoftwaresubs.push(rootName); 
         // }
     }
-    var sofwareTree = {"Software": tree}
-    return(sofwareTree)
+    console.log("ORIGINAL")
+    console.log(tree)
+    console.log("--------")
+    var sofwareTree = {"label":"SOFTWARE" , "isRoot":true }
+    for (item in tree){
+        var children = {}
+        children.children = {}
+        var children = {}
+        for (child in tree[item]){
+            var key = Object.keys(tree[item][child])[0];
+            children[key] =  tree[item][child][key]
+        }
+        console.log(children)
+        sofwareTree["children"] = children
+        console.log(sofwareTree)
+        break  
+    }
+
+    result = { "software":sofwareTree }
+
+
+    return(result)
     // console.log(tree)
 
 }
